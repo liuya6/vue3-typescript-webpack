1c8a12e70b24547e8e27d6cf2bd8b5b69357d745
刘亚泉 <liu15107128281@gmail.com>
2021-07-08 22:57:44 +0800

    polyfill scroll

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "reset-css";
 import "./style/index.less";
+import "./utils/smoothscroll.min";
 import "amfe-flexible";
 
 import router from "./router/index";
